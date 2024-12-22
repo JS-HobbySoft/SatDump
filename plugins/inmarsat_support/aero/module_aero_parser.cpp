77b7cec57a5c2846557783ed44f24718cc506f15
JS-HobbySoft <111251869+JS-HobbySoft@users.noreply.github.com>
2024-12-22 08:28:14 -0600

    Update module_aero_parser.cpp
    SSU and AES System Broadcast Index messages will neither be saved to file nor printed to the terminal

--- plugins/inmarsat_support/aero/module_aero_parser.cpp
+++ plugins/inmarsat_support/aero/module_aero_parser.cpp
@@ -8,6 +8,10 @@
 #include "decode_utils.h"
 #include "common/dsp/io/wav_writer.h"
 #include "common/audio/audio_sink.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #define SIGNAL_UNIT_SIZE_BYTES 12
 
@@ -15,6 +19,7 @@ namespace inmarsat
 {
     namespace aero
     {
+        std::vector<std::string> stringList = {"SSU", "AES System Table Broadcast (Index)", "Reserved 0x26"};
         AeroParserModule::AeroParserModule(std::string input_file, std::string output_file_hint, nlohmann::json parameters) : ProcessingModule(input_file, output_file_hint, parameters)
         {
             buffer = new uint8_t[SIGNAL_UNIT_SIZE_BYTES];
@@ -119,14 +124,16 @@ namespace inmarsat
                                            (timeReadable->tm_min > 9 ? std::to_string(timeReadable->tm_min) : "0" + std::to_string(timeReadable->tm_min)) +             // Minutes mm
                                            (timeReadable->tm_sec > 9 ? std::to_string(timeReadable->tm_sec) : "0" + std::to_string(timeReadable->tm_sec)) + "Z";        // Seconds ss
 
-                std::string path = directory + "/" + utc_filename + ".json";
-                int i = 1;
-                while (std::filesystem::exists(path))
-                    path = directory + "/" + utc_filename + "_" + std::to_string(i++) + ".json";
+                if (std::find(stringList.begin(), stringList.end(), pkt_name) == stringList.end()) {
+                    std::string path = directory + "/" + utc_filename + ".json";
+                    int i = 1;
+                    while (std::filesystem::exists(path))
+                        path = directory + "/" + utc_filename + "_" + std::to_string(i++) + ".json";
 
-                std::ofstream outf(path, std::ios::binary);
-                outf << msg.dump(4);
-                outf.close();
+                    std::ofstream outf(path, std::ios::binary);
+                    outf << msg.dump(4);
+                    outf.close();
+                }
             }
         }
 
@@ -200,9 +207,11 @@ namespace inmarsat
                     if (!final_pkt.contains("msg_name"))
                     {
                         std::string name = pkt_type_to_name(pkt_id);
-                        if (name.find("Reserved") == std::string::npos)
-                            final_pkt["msg_name"] = name;
-                        logger->info("Packet : " + name);
+                        if (std::find(stringList.begin(), stringList.end(), name) == stringList.end()) {
+                            if (name.find("Reserved") == std::string::npos)
+                                final_pkt["msg_name"] = name;
+                            logger->info("Packet : " + name);
+                        }
                     }
 
                     final_pkt["timestamp"] = time(0);
